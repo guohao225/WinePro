0f6982f01287b8292c02a062e46894eafedc647c
guohao225 <guohao225@163.com>
2021-10-07 17:56:25 +0800

    first

---
+++ src/main/java/com/guohao/wine_h/WineHApplication.java
@@ -0,0 +1,15 @@
+package com.guohao.wine_h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineHApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WineHApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guohao/wine_h/WineHApplicationTests.java
@@ -0,0 +1,15 @@
+package com.guohao.wine_h;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineHApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
